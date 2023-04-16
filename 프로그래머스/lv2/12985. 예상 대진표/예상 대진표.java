3f9b7cd4750a4b1c6072766a8c223648587863bf
devdabong <78587167+devdabong@users.noreply.github.com>
2023-04-16 17:37:30 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -3,7 +3,7 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        for (int i = 0; Math.pow(2,i) <= n; i++) {
+        for (int i = 0; Math.pow(2,i) < n; i++) {
             a = (a/2) + (a%2);
             b = (b/2) + (b%2);
             answer++;

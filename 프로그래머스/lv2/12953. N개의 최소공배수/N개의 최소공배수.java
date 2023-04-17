76db96a793388295773ee1506fdf39e52d15c0a5
devdabong <78587167+devdabong@users.noreply.github.com>
2023-04-17 15:16:30 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.41 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int calGcd(int a, int b) {
+        int big = Math.max(a, b);
+        int small = Math.min(a, b);
+        int n = 0;
+        while (big % small > 0) {
+            n = big % small;
+            big = small;
+            small = n;
+        }
+        return small;
+    }
+    public int solution(int[] arr) {
+        int answer = 0;
+        
+        Arrays.sort(arr);
+        int gcd = calGcd(arr[0], arr[1]);
+        int lcm = arr[0] * arr[1] / gcd;
+        
+        for (int i = 2; i < arr.length; i++) {
+            int a = lcm;
+            int b = arr[i];
+            //System.out.println("a : " + a);
+            //System.out.println("b : " + b);
+            gcd = calGcd(a, b);
+            lcm = a * b / gcd;
+            //System.out.println("gcd : " + gcd);
+            //System.out.println("lcm : " + lcm);
+        }
+        answer = lcm;
+        
+        return answer;
+    }
+}
